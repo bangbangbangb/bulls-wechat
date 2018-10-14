2567621145c6da4f67188caf361db8a5dd3be1b0
grace <1429061040@qq.com>
2018-10-14 20:12:59 +0800

    将主页的样式设置好了，视频的那一页还需再看看文档才知道怎么写下去，暂时先是一个list吧

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@
 //获取应用实例
 Page({
   data: {
-    swiperName:['[魅力人生]','2','3'],
+    swiperName:['[魅力人生]#$#$@@@','2','3'],
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
@@ -11,7 +11,26 @@ Page({
     indicatorDots: false,
     autoplay: true,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    videoImage:[
+      { 
+        image: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        desc: "第一张图片"
+      },
+      {
+        image: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        desc: "第二张图片"
+      },
+      {
+        image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        desc: "第三张图片"
+      },
+      {
+        image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        desc: "第四张图片"
+      }
+
+    ]
   },
   changeIndicatorDots: function (e) {
     this.setData({
